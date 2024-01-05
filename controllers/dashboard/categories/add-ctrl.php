8e48ada2125bd2729b2a4c6bdf8c682484ceb998
Kelaven <kevin.lavenant@live.fr>
2024-01-05 13:50:16 +0100

    Form Category commencé

--- controllers/dashboard/categories/add-ctrl.php
+++ controllers/dashboard/categories/add-ctrl.php
@@ -4,3 +4,10 @@ $host = 'localhost';
 $dbname = 'rentmyride';
 $username = 'rentmyride_admin';
 $password = '<REMOVED>';
+
+
+// views
+
+include __DIR__.'/../../../views/templates/dashboard/header.php';
+include __DIR__.'/../../../views/dashboard/categories/add.php';
+include __DIR__.'/../../../views/templates/dashboard/footer.php';
