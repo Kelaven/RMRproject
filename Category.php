63e142fe561a73649dc2ccb532d27b145ee506fb
Kelaven <kevin.lavenant@live.fr>
2024-01-05 09:35:15 +0100

    projet commencé

---
+++ Category.php
@@ -0,0 +1,27 @@
+<?php
+
+// * création de la classe
+class Category{
+
+    // * création des attributs
+    private string $name;
+
+    // * création des getters
+    /**
+     * retourne la valeur de name
+     * @return string
+     */
+    public function get_name(): string{
+        return $this->name;
+    }
+    // * création des setters
+    /**
+     * modifies la valeur de name
+     * @param string $name
+     * 
+     * @return [type]
+     */
+    public function set_name(string $name){
+        $this->name = $name;
+    }
+}
