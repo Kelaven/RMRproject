c214e3d9a0ed955550d4e5708e27e2adfb002526
Kelaven <kevin.lavenant@live.fr>
2024-01-08 16:46:49 +0100

    slide list ok

--- config/init.php
+++ config/init.php
@@ -4,3 +4,11 @@
 // ! regex
 
 define('REGEX_CATEGORY', '^.{2,30}$');
+
+
+
+// ! config
+
+define('DSN', 'mysql:dbname=rentmyride;host=localhost');
+define('USER', 'rentmyride_admin');
+define('PASSWORD', '<REMOVED>');
